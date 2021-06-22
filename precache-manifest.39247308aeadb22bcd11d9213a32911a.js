7ba1ffbece3950bcc059696e473c73a4a18e6cb1
koyu <me@koyu.space>
2021-06-22 10:35:03 +0200

    Updates

---
+++ precache-manifest.39247308aeadb22bcd11d9213a32911a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d3bbe28035063fd656b3fbb2ebc351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b802ba78dc37e7241ba",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "0f3f1a402e7545642576",
+    "url": "/static/css/main.a57e710f.chunk.css"
+  },
+  {
+    "revision": "6b802ba78dc37e7241ba",
+    "url": "/static/js/2.e145d807.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/static/js/2.e145d807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3f1a402e7545642576",
+    "url": "/static/js/main.62a64622.chunk.js"
+  },
+  {
+    "revision": "78d516874e2cb9bf8737",
+    "url": "/static/js/runtime-main.7d604d2e.js"
+  }
+]);
